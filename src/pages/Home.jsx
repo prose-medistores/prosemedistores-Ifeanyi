b587762b258f7e36c51194e1db14df538dedf243
Charles <adeakinyemi129@gmail.com>
2025-10-31 14:35:25 +0100

    testing deploy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,7 @@ export default function Home() {
         <section id="shop" className="py-12 bg-light">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-2xl font-semibold text-primary mb-6">
-          Available Medicines
+          Available Medicinez
         </h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6">
           {products.length > 0 ? (
